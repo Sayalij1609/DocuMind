c343b72920f27f51c3ecb68bb6e04a4e8489125f
sayalij1609 <jadhavsayali489@gmail.com>
2026-08-27 12:27:30 +0530

    Update main.py by Adding Cross-Origin Resource Sharing (CORS) through FastAPI Middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.config import settings
 from app.api.document_routes import router as document_router
+
 
 app = FastAPI(
     title=settings.app_name,
@@ -11,7 +14,22 @@ app = FastAPI(
     )
 )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(document_router)
+
+
 @app.get("/")
 async def root():
     return {
@@ -19,6 +37,7 @@ async def root():
         "version": settings.app_version,
         "status": "running"
     }
+
 
 @app.get("/health")
 async def health_check():
